cd0820ddc9882c4c5b038dc2854da44663dedacf
unknown <milagos31@gmail.com>
2023-07-03 19:01:04 +0800

    generated folder structure and mockup data

--- src/mock/admins.js
+++
@@ -1,9 +0,0 @@
-export const admins = [
-    {
-        email: "bir@bir.gov.ph",
-        password: "123456",
-        address: "molo, iloilo citu",
-        agency: "Bureau of Internal Revenue",
-        queues: [{ name: "Window 1", updatedOn: "2023-04-23 15:33:10", queue: "43" }],
-    },
-];
