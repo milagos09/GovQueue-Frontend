fa5486e20df97a982e49b30c2797b459afe9afd6
unknown <milagos31@gmail.com>
2023-07-07 17:28:43 +0800

    added material-ui in dependency

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,12 @@
 import "./App.css";
-import admins from "../fake/admins.json";
+// import admins from "../fake/admins.json";
 
 export default function App() {
     return (
         <>
-            {admins.map((e) => (
-                <p key={e.id}>{e.agency}</p>
-            ))}
+            {/* <Header/> */}
+            {/* <Body/> */}
+            {/* <Footer/> */}
         </>
     );
 }
