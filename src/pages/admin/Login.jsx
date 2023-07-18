9ddd8aa845ff02a8d5a2970efa72fbddcddfe3df
johanblupojr <johanblupojr@gmail.com>
2023-07-18 22:46:46 +0800

    Create Admin Login Page

--- src/pages/admin/Login.jsx
+++ src/pages/admin/Login.jsx
@@ -1,10 +1,53 @@
+import Box from "@mui/material/Box";
 import AdminLogin from "../../components/Login/index";
+import NavBar from "../../components/Navbar";
+import Grid from "@mui/material/Unstable_Grid2";
 
 export default function Login() {
   return (
     <>
-      <AdminLogin />
-      <h2>Login Page</h2>;
+      <NavBar />
+      <Box overflow="hidden">
+        <Grid
+          paddingY={"19.7vh"}
+          rowSpacing={5}
+          alignItems="center"
+          container
+          height="100%"
+          flexDirection={{ xs: "column", sm: "row" }}
+          justifyContent={{ xs: "center", sm: "center" }}
+        >
+          <Grid
+            item
+            sm={8}
+            xs={12}
+            order={{ xs: 2, sm: 1 }}
+            justifyContent={{ xs: "center", sm: "center" }}
+          >
+            <AdminLogin />
+          </Grid>
+          <Grid
+            item
+            sm={4}
+            xs={12}
+            order={{ xs: 1, sm: 2 }}
+            justifyContent="center"
+          >
+            <Box
+              display="flex"
+              justifyContent="flex-start"
+              width={400}
+              height={400}
+            >
+              <img
+                src="src\assets\govQ-logo.png"
+                alt=""
+                style={{ width: "100%", height: "100%" }}
+              />
+            </Box>
+          </Grid>
+        </Grid>
+      </Box>
     </>
   );
 }
