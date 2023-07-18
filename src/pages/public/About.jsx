48b806fac0f60ba9a36a3d61997cef234ad20e29
420ku <mordecai51213@gmail.com>
2023-07-18 20:48:44 +0800

    revised about us page

--- src/pages/public/About.jsx
+++ src/pages/public/About.jsx
@@ -1,10 +1,82 @@
 import NavBar from "../../components/Navbar";
+import Box from '@mui/material/Box';
+import Grid from '@mui/material/Unstable_Grid2';
+import { styled } from '@mui/material/styles';
+import Paper from '@mui/material/Paper';
+import Chip from '@mui/material/Chip';
+import Divider from '@mui/material/Divider';
 
 export default function About() {
+    const Item = styled(Paper)(({ theme }) => ({
+        backgroundColor: theme.palette.mode === 'dark' ? '#1A2027' : '#fff',
+        ...theme.typography.body2,
+        padding: theme.spacing(2),
+        textAlign: 'center',
+        color: theme.palette.text.secondary,
+      }));
     return (
         <>
             <NavBar />
-            <h2>About us Page</h2>
-        </>
+            <Box sx={{ flexGrow: 1 }}>
+      <Grid container spacing={3}>
+        <Grid xs>
+          <Item>The quick brown fox jumps over the lazy dog</Item>
+        </Grid>
+        <Grid xs={6}>
+          <Item>The quick brown fox jumps over the lazy dog</Item>
+        </Grid>
+        <Grid xs>
+          <Item>The quick brown fox jumps over the lazy dog</Item>
+        </Grid>
+      </Grid>
+    </Box>
+
+  
+    
+    <Divider>
+        <Chip label="" />
+      </Divider>
+      
+     
+
+    <Box sx={{ flexGrow: 1 }}>
+      <Grid container spacing={3}>
+        <Grid xs>
+          <Item>The quick brown fox jumps over the lazy dog</Item>
+        </Grid>
+      </Grid>
+    </Box>
+
+    <Divider>
+        <Chip label="" />
+      </Divider>
+
+    <Box sx={{ flexGrow: 1 }}>
+      <Grid container spacing={3}>
+        <Grid xs>
+          <Item>The quick brown fox jumps over the lazy dog</Item>
+        </Grid>
+      </Grid>
+    </Box>
+
+    <Divider>
+        <Chip label="" />
+      </Divider>
+    
+    <Box sx={{ flexGrow: 1 }}>
+      <Grid container spacing={3} justifyContent="center">
+        <Grid xs={2}>
+          <Item>The quick brown fox jumps over the lazy dog</Item>
+        </Grid>
+        <Grid xs={2}>
+          <Item>The quick brown fox jumps over the lazy dog</Item>
+        </Grid>
+        <Grid xs={2}>
+          <Item>The quick brown fox jumps over the lazy dog!</Item>
+        </Grid>
+      </Grid>
+    </Box>
+    
+    </>
     );
 }
--- src/pages/public/Support.jsx
+++ src/pages/public/Support.jsx
@@ -1,83 +1,10 @@
 import NavBar from "../../components/Navbar";
-import Box from '@mui/material/Box';
-import Grid from '@mui/material/Unstable_Grid2';
-import { styled } from '@mui/material/styles';
-import Paper from '@mui/material/Paper';
-import Chip from '@mui/material/Chip';
-import Divider from '@mui/material/Divider';
-
 
 export default function Support() {
-    const Item = styled(Paper)(({ theme }) => ({
-        backgroundColor: theme.palette.mode === 'dark' ? '#1A2027' : '#fff',
-        ...theme.typography.body2,
-        padding: theme.spacing(2),
-        textAlign: 'center',
-        color: theme.palette.text.secondary,
-      }));
-    return (
+        return (
         <>
             <NavBar />
-            <Box sx={{ flexGrow: 1 }}>
-      <Grid container spacing={3}>
-        <Grid xs>
-          <Item>The quick brown fox jumps over the lazy dog</Item>
-        </Grid>
-        <Grid xs={6}>
-          <Item>The quick brown fox jumps over the lazy dog</Item>
-        </Grid>
-        <Grid xs>
-          <Item>The quick brown fox jumps over the lazy dog</Item>
-        </Grid>
-      </Grid>
-    </Box>
-
-  
-    
-    <Divider>
-        <Chip label="" />
-      </Divider>
-      
-     
-
-    <Box sx={{ flexGrow: 1 }}>
-      <Grid container spacing={3}>
-        <Grid xs>
-          <Item>The quick brown fox jumps over the lazy dog</Item>
-        </Grid>
-      </Grid>
-    </Box>
-
-    <Divider>
-        <Chip label="" />
-      </Divider>
-
-    <Box sx={{ flexGrow: 1 }}>
-      <Grid container spacing={3}>
-        <Grid xs>
-          <Item>The quick brown fox jumps over the lazy dog</Item>
-        </Grid>
-      </Grid>
-    </Box>
-
-    <Divider>
-        <Chip label="" />
-      </Divider>
-    
-    <Box sx={{ flexGrow: 1 }}>
-      <Grid container spacing={3} justifyContent="center">
-        <Grid xs={2}>
-          <Item>The quick brown fox jumps over the lazy dog</Item>
-        </Grid>
-        <Grid xs={2}>
-          <Item>The quick brown fox jumps over the lazy dog</Item>
-        </Grid>
-        <Grid xs={2}>
-          <Item>The quick brown fox jumps over the lazy dog!</Item>
-        </Grid>
-      </Grid>
-    </Box>
-    
+            <h2>Support Page</h2>
     </>
     );
 }
