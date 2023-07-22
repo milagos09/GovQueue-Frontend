a3518a9e91ae6bb870ffc0ec127cc5fddc1f73e7
unknown <milagos31@gmail.com>
2023-07-22 17:59:18 +0800

    fixed responsiveness

--- src/pages/admin/Login.jsx
+++ src/pages/admin/Login.jsx
@@ -3,52 +3,41 @@ import AdminLogin from "../../components/Login/index";
 import NavBar from "../../components/Navbar";
 import Grid from "@mui/material/Unstable_Grid2";
 import BannerLogo from "../../assets/govQ-logo.png";
+import { CheckScreenSize } from "./../../hooks/CheckScreenSize";
 
 export default function Login() {
-  return (
-    <>
-      <NavBar />
-      <Box overflow="hidden">
-        <Grid
-          paddingY={"19.7vh"}
-          rowSpacing={5}
-          alignItems="center"
-          container
-          height="100%"
-          flexDirection={{ xs: "column", sm: "row" }}
-          justifyContent={{ xs: "center", sm: "center" }}
-        >
-          <Grid
-            item
-            sm={8}
-            xs={12}
-            order={{ xs: 2, sm: 1 }}
-            justifyContent={{ xs: "center", sm: "center" }}
-          >
-            <AdminLogin />
-          </Grid>
-          <Grid
-            item
-            sm={4}
-            xs={12}
-            order={{ xs: 1, sm: 2 }}
-            justifyContent="center"
-          >
-            <Box
-              display="flex"
-              justifyContent="flex-start"
-              width={400}
-              height={400}
-            >
-              <img
-                src={BannerLogo}
-                alt="BannerLogo"
-                style={{ width: "100%", height: "100%" }}
-              />
+    const { width, height } = CheckScreenSize();
+    return (
+        <>
+            <NavBar />
+            <Box>
+                <Grid
+                    alignItems="center"
+                    container
+                    height="100%"
+                    flexDirection={{ xs: "column", sm: "row" }}
+                    justifyContent={{ xs: "center", sm: "center" }}
+                    padding="20px"
+                    columnSpacing={3}
+                    sx={{ minHeight: width > 600 ? `${height - 172}px` : "0" }}
+                >
+                    <Grid item sm={7} xs={12} order={{ xs: 2, sm: 1 }} justifyContent={{ xs: "center", sm: "center" }}>
+                        <AdminLogin />
+                    </Grid>
+                    <Grid item sm={5} xs={12} order={{ xs: 1, sm: 2 }} textAlign="center" paddingBottom={3}>
+                        <img
+                            src={BannerLogo}
+                            alt="BannerLogo"
+                            style={{
+                                width: "100%",
+                                aspectRatio: "1/1",
+                                maxHeight: width < 600 ? "250px" : "450px",
+                                maxWidth: width < 600 ? "250px" : "450px",
+                            }}
+                        />
+                    </Grid>
+                </Grid>
             </Box>
-          </Grid>
-        </Grid>
-      </Box>
-    </>
-  );
+        </>
+    );
 }
