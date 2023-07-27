2cab1f0040f9a3f8dfaf6028b5e52386ecaab3ff
420ku <mordecai51213@gmail.com>
2023-07-27 18:13:58 +0800

    added content and fixed layout

--- src/pages/public/Support.jsx
+++ src/pages/public/Support.jsx
@@ -14,14 +14,14 @@ export default function Support() {
   const Item1 = styled(Paper)(({ theme }) => ({
     backgroundColor: theme.palette.mode === "dark" ? "#1A2027" : "#fff",
     ...theme.typography.body2,
-    padding: theme.spacing(1),
+    padding: theme.spacing(0),
     textAlign: "left",
     color: theme.palette.text.secondary,
   }));
   const Item2 = styled(Paper)(({ theme }) => ({
     backgroundColor: theme.palette.mode === "dark" ? "#1A2027" : "#fff",
     ...theme.typography.body2,
-    padding: theme.spacing(1),
+    padding: theme.spacing(0),
     textAlign: "left",
     color: theme.palette.text.secondary,
   }));
@@ -29,9 +29,9 @@ export default function Support() {
   return (
     <>
       <NavBar />
-      <Box xs="2" sx={{ width: "100%" }}>
-        <Grid container rowSpacing={2} columnSpacing={{ xs: 0, sm: 2, md: 2 }}>
-          <Grid xs={9} sm={3} sx={{ border: "none" }}>
+      <Box xs="2" sx={{ width: "100%", overflowX: "hidden" }}>
+        <Grid container rowSpacing={2} columnSpacing={{ xs: 2, sm: 2, md: 2 }}>
+          <Grid xs={12} sm={4}>
             <Item1>
               <Box
                 sx={{
@@ -66,7 +66,13 @@ export default function Support() {
                               What is GovQueue?
                             </ListItem>
                             <ListItem disablePadding sx={{ px: 1 }}>
-                              How to use the App?
+                              How can a Filipino Citizen benefit from this App?
+                            </ListItem>
+                            <ListItem disablePadding sx={{ px: 1 }}>
+                              How can a Government Agency benefit from this App?
+                            </ListItem>
+                            <ListItem disablePadding sx={{ px: 1 }}>
+                              Can I use this App for free?
                             </ListItem>
                           </List>
                         </nav>
@@ -86,7 +92,7 @@ export default function Support() {
                   justifyContent: "flex-start",
                 }}
               >
-                <Grid item xs={12} md={9} lg={12} sx={{ px: 1, py: 0 }}>
+                <Grid item xs={12} sm={8} lg={12} sx={{ px: 1, py: 0 }}>
                   <Card
                     variant="outlined"
                     sx={{ minWidth: 275, height: "100%", border: "none" }}
@@ -96,13 +102,100 @@ export default function Support() {
                         sx={{ fontSize: 30, py: 0, px: 0 }}
                         gutterBottom
                       >
-                        GovQueue FAQ
+                        GovQueue Frequently Asked Questions
                       </Typography>
                       <Typography
-                        sx={{ fontSize: 22, py: 0, px: 0 }}
+                        sx={{ fontSize: 20, py: 0, px: 0 }}
                         gutterBottom
                       >
                         What is GovQueue?
+                      </Typography>
+                      <Typography
+                        sx={{ fontSize: 14, py: 0, px: 0 }}
+                        gutterBottom
+                      >
+                        It is a web application that is a robust and
+                        user-friendly queuing system designed to improve
+                        customer flow management and service delivery for
+                        government agencies. It includes a centralized dashboard
+                        for real-time visibility, separate admin portals for
+                        agencies to manage their queues, a public-facing
+                        interface for citizens to view queue statuses and wait
+                        times, and appointment scheduling. The system also
+                        offers reporting and analytics capabilities to enable
+                        evidence-based decision-making and integration with
+                        existing government systems for seamless data exchange.
+                      </Typography>
+                      <Typography
+                        sx={{ fontSize: 20, py: 0, px: 0 }}
+                        gutterBottom
+                      >
+                        How can a Filipino Citizen benefit from this App?
+                      </Typography>
+                      <Typography
+                        sx={{ fontSize: 14, py: 0, px: 0 }}
+                        gutterBottom
+                      >
+                        The queuing system application offers various benefits
+                        to end users interacting with government agencies. These
+                        advantages include reduced waiting times through
+                        real-time queue statuses and estimated wait times,
+                        improved visit planning with accurate queue information
+                        and appointment scheduling, mobile accessibility for
+                        on-the-go access, and SMS notifications for staying
+                        informed. The application promotes transparency and a
+                        user-friendly interface, leading to an enhanced customer
+                        experience and confidence in efficient service delivery.
+                        The overall result is significant time savings for end
+                        users when completing tasks at government agencies.
+                      </Typography>
+                      <Typography
+                        sx={{ fontSize: 20, py: 0, px: 0 }}
+                        gutterBottom
+                      >
+                        How can a Government Agency benefit from this App?
+                      </Typography>
+                      <Typography
+                        sx={{ fontSize: 14, py: 0, px: 0 }}
+                        gutterBottom
+                      >
+                        The queuing system application offers several benefits
+                        to government agencies. It enables efficient resource
+                        allocation by providing real-time visibility of queue
+                        statuses, leading to optimal utilization of staff and
+                        service counters. The application improves service
+                        delivery by reducing waiting times and streamlining the
+                        queuing process, enhancing citizen satisfaction and
+                        perception of agency efficiency. Through reporting and
+                        analytics capabilities, agencies can make data-driven
+                        decisions, adjusting operations and services as needed.
+                        Features like appointment scheduling and SMS
+                        notifications enhance citizen engagement, trust, and
+                        convenience. The application promotes transparency,
+                        streamlines administration, potentially leading to cost
+                        savings, and integrates with existing systems for
+                        increased efficiency. It is also scalable, adaptable,
+                        and facilitates compliance monitoring for improved
+                        service standards.
+                      </Typography>
+                      <Typography
+                        sx={{ fontSize: 20, py: 0, px: 0 }}
+                        gutterBottom
+                      >
+                        Can I use this app for free?
+                      </Typography>
+                      <Typography
+                        sx={{ fontSize: 14, py: 0, px: 0 }}
+                        gutterBottom
+                      >
+                        YES! This application is available for free, but
+                        donations are encouraged to support its growth and
+                        improvement. Donations would be used to enhance server
+                        capacity, improve app performance, hire more developers
+                        for new features, and increase marketing efforts to
+                        reach a broader audience. Any donation, regardless of
+                        the amount, would make a meaningful difference and is
+                        greatly appreciated as a valuable form of support.
                       </Typography>
                     </CardContent>
                   </Card>
@@ -112,7 +205,6 @@ export default function Support() {
           </Grid>
         </Grid>
       </Box>
-
       <FacebookMessengerChat />
     </>
   );
