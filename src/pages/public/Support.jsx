c1c808073271f1e3a9fe9c0a22b352d81e25cb6f
420ku <mordecai51213@gmail.com>
2023-07-27 19:34:34 +0800

    revised messenger plugin

--- src/pages/public/Support.jsx
+++ src/pages/public/Support.jsx
@@ -1,5 +1,5 @@
 import NavBar from "../../components/Navbar";
-import FacebookMessengerChat from "../../components/FacebookMessengerChat";
+import App from "../../components/FacebookMessengerChat";
 import { styled } from "@mui/material/styles";
 import Grid from "@mui/material/Unstable_Grid2";
 import Paper from "@mui/material/Paper";
@@ -219,7 +219,7 @@ export default function Support() {
           </Grid>
         </Grid>
       </Box>
-      <FacebookMessengerChat pageId="108965818922829" />
+      <App pageId="108965818922829" />
     </>
   );
 }
