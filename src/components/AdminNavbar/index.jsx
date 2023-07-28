29bf9083932d090ae462f20fb13da230d218a162
420ku <mordecai51213@gmail.com>
2023-07-28 10:44:04 +0800

    create admin navbar

--- src/components/AdminNavbar/index.jsx
+++ src/components/AdminNavbar/index.jsx
@@ -1,16 +1,18 @@
 import AppBar from "@mui/material/AppBar";
 import Toolbar from "@mui/material/Toolbar";
 import Container from "@mui/material/Container";
+import AdminNavLink from "./AdminNavLink";
+import AdminNavBarAppName from "./AdminNavBarAppName";
+import AdminHamburgerMenu from "./AdminHamburgerMenu";
 
 export default function AdminNavBar() {
   return (
     <AppBar position="sticky">
       <Container>
         <Toolbar disableGutters>
-          {/* <HamburgerMenu />
-                    <AppName />
-                    <NavLink />
-                    <FormatDateTime /> */}
+          <AdminHamburgerMenu />
+          <AdminNavBarAppName />
+          <AdminNavLink />
         </Toolbar>
       </Container>
     </AppBar>
