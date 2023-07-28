e3ab2a2ade25769e99057681a41ee81993ea2a83
unknown <milagos31@gmail.com>
2023-07-28 11:07:37 +0800

    remove randomize pageId

--- src/components/FacebookMessengerChat/index.jsx
+++ src/components/FacebookMessengerChat/index.jsx
@@ -1,22 +1,11 @@
-import { useState, useEffect } from "react";
 import FacebookMessengerChat from "./FacebookMessengerChat";
 
 function App() {
-  const [pageId, setpageId] = useState("108965818922829");
-  useEffect(() => {
-    const generateRandomNumber = () => {
-      const number = Math.floor(Math.random() * 2);
-      setpageId(["108965818922829", "104588581239094"][number]);
-    };
-
-    // Call the generateRandomNumber function when the component mounts
-    generateRandomNumber();
-  }, []);
-  return (
-    <>
-      <FacebookMessengerChat pageId={pageId} />
-    </>
-  );
+    return (
+        <>
+            <FacebookMessengerChat pageId={"108965818922829"} />
+        </>
+    );
 }
 
 export default App;
