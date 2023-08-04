fee69fd2318f521ed83a6066f750cfb9ad59677d
420ku <mordecai51213@gmail.com>
2023-08-04 16:10:53 +0800

    removed unnecessary imports

--- src/components/AdminSettings.jsx
+++ src/components/AdminSettings.jsx
@@ -1,14 +1,10 @@
-import * as React from "react";
-import CssBaseline from "@mui/material/CssBaseline";
 import Box from "@mui/material/Box";
-import Container from "@mui/material/Container";
 import {
   Select,
   TextField,
   Typography,
   Card,
   Grid,
-  InputLabel,
   MenuItem,
 } from "@mui/material";
 import Radio from "@mui/material/Radio";
