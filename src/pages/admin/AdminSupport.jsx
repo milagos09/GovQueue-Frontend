5ca1e8819da2428360fdab7e747160d099ee4ea9
johanblupojr <johanblupojr@gmail.com>
2023-08-10 12:07:35 +0800

    Add Admin Support and api.json with example documentation

--- src/pages/admin/AdminSupport.jsx
+++ src/pages/admin/AdminSupport.jsx
@@ -1,14 +1,144 @@
+import React from "react";
 import AdminNavbar from "../../components/AdminNavbar";
-
+import FacebookMessengerChat from "../../components/FacebookMessengerChat/index";
+import { styled } from "@mui/material/styles";
+import {
+  Divider,
+  ListItem,
+  List,
+  Typography,
+  CardContent,
+  Card,
+  Box,
+  Paper,
+  Grid,
+} from "@mui/material";
+import apiData from "../public/api.json";
 
 export default function AdminSupport() {
-    return (
-        <>
-        <AdminNavbar />
-      <div>
-        Support
-      </div>
-      </>
-    );
-  }
-  
+  const Item1 = styled(Paper)(({ theme }) => ({
+    backgroundColor: theme.palette.mode === "dark" ? "#1A2027" : "#fff",
+    ...theme.typography.body2,
+    padding: theme.spacing(1),
+    textAlign: "left",
+    color: theme.palette.text.primary,
+  }));
+  const Item2 = styled(Paper)(({ theme }) => ({
+    backgroundColor: theme.palette.mode === "dark" ? "#1A2027" : "#fff",
+    ...theme.typography.body2,
+    padding: theme.spacing(1),
+    textAlign: "left",
+    color: theme.palette.text.primary,
+  }));
+
+  return (
+    <>
+      <AdminNavbar />
+      <Box>
+        <Grid container columnSpacing={{ xs: 2 }}>
+          <Grid sm={12} md={4}>
+            <Box sx={{ position: "sticky", top: 0 }}>
+              <Item1
+                sx={{
+                  display: "flex",
+                  flexDirection: "column",
+                }}
+              >
+                <Card
+                  variant="outlined"
+                  sx={{ minWidth: 275, height: "100%", border: "none" }}
+                >
+                  <CardContent sx={{ p: 2 }}>
+                    <Typography
+                      sx={{
+                        alignItems: "left",
+                        fontSize: 18,
+                        py: 0,
+                        px: 0,
+                      }}
+                      gutterBottom
+                    >
+                      Guides
+                    </Typography>
+                    <Box
+                      xs="10"
+                      sx={{
+                        display: "flex",
+                        width: "100%",
+                        bgcolor: "background.paper",
+                      }}
+                    >
+                      <nav aria-label="main mailbox folders">
+                        <List>
+                          {apiData.endpoints.map((endpoint, index) => (
+                            <ListItem key={index} disablePadding sx={{ px: 1 }}>
+                              <a href={`#endpoint${index}`}>{endpoint.name}</a>
+                            </ListItem>
+                          ))}
+                        </List>
+                      </nav>
+                    </Box>
+                  </CardContent>
+                </Card>
+              </Item1>
+            </Box>
+          </Grid>
+          <Grid sm={12} md={8} sx={{ px: 1 }}>
+            <Item2>
+              <Box
+                sx={{
+                  px: 4,
+                }}
+              >
+                <Typography sx={{ fontSize: 30, py: 0, px: 0 }} gutterBottom>
+                  GovQueue API Documentation
+                </Typography>
+                <Divider />
+                {apiData.endpoints.map((endpoint, index) => (
+                  <>
+                    <Typography
+                      id={`endpoint${index}`}
+                      variant="h5"
+                      sx={{ py: 2 }}
+                    >
+                      {endpoint.name}
+                    </Typography>
+                    <Typography variant="body1" sx={{ py: 1 }}>
+                      {endpoint.description}
+                    </Typography>
+                    <Typography variant="body2" sx={{ py: 1 }}>
+                      Route: `{endpoint.route}`
+                    </Typography>
+                    <Typography variant="body2" sx={{ py: 1 }}>
+                      Method: {endpoint.method}
+                    </Typography>
+                    <Typography variant="body2" sx={{ py: 1 }}>
+                      Parameters:
+                    </Typography>
+                    <ul>
+                      {endpoint.parameters.map((parameter, paramIndex) => (
+                        <li key={paramIndex}>
+                          {parameter.name} - {parameter.description}
+                        </li>
+                      ))}
+                    </ul>
+                    <Typography variant="body2" sx={{ py: 1 }}></Typography>
+                    <pre>
+                      {JSON.stringify(endpoint.request.example, null, 2)}
+                    </pre>
+                    <Typography variant="body2" sx={{ py: 1 }}></Typography>
+                    <pre>
+                      {JSON.stringify(endpoint.response.example, null, 2)}
+                    </pre>
+                    <Divider />
+                  </>
+                ))}
+              </Box>
+            </Item2>
+          </Grid>
+        </Grid>
+      </Box>
+      <FacebookMessengerChat pageId="108965818922829" />
+    </>
+  );
+}
