af1bde90b672d57d1ca7ad7bbeb2af33a0359813
unknown <milagos31@gmail.com>
2023-08-18 17:25:43 +0800

    add error page when admin is undefined

--- src/pages/public/Agency.jsx
+++ src/pages/public/Agency.jsx
@@ -8,25 +8,30 @@ import Navbar from "./../../components/Navbar";
 import Announcement from "../../components/AgencyComponents/Announcement";
 import { useParams } from "react-router-dom";
 import FacebookMessengerChat from "./../../components/FacebookMessengerChat";
+import ErrorPage from "./../ErrorPage";
 
 export default function Agency() {
     const id = Number(useParams().id);
-    const admin = admins.find((admin) => admin.id === id) || [];
+    const admin = admins.find((admin) => admin.id === id) || {};
 
     return (
         <>
             <Navbar />
-            <Container>
-                <Announcement show={admin.showAnnouncement} announcement={admin.announcement} />
-                <Grid container>
-                    <Grid item xs={12} sm={6} md={5}>
-                        <Profile admin={admin} />
+            {admin.id != undefined ? (
+                <Container>
+                    <Announcement show={admin.showAnnouncement} announcement={admin.announcement} />
+                    <Grid container>
+                        <Grid item xs={12} sm={6} md={5}>
+                            <Profile admin={admin} />
+                        </Grid>
+                        <Grid item xs={12} sm={6} md={7}>
+                            <Queues queue={queues.filter((q) => id === q.adminId)} />
+                        </Grid>
                     </Grid>
-                    <Grid item xs={12} sm={6} md={7}>
-                        <Queues queue={queues.filter((q) => id === q.adminId)} />
-                    </Grid>
-                </Grid>
-            </Container>
+                </Container>
+            ) : (
+                <ErrorPage />
+            )}
             {admin.showMessenger && <FacebookMessengerChat pageId={admin.messengerId} />}
         </>
     );
