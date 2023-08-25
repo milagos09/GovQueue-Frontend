f1e7e3376669ff5602461585375a8c46179bb0b7
420ku <mordecai51213@gmail.com>
2023-08-25 09:56:09 +0800

    Merge branch 'GCQS-52' of github.com:milagos09/GovQueue-Frontend into GCQS-53

--- src/components/AdminHome/AdminHeader.jsx
+++ src/components/AdminHome/AdminHeader.jsx
@@ -19,7 +19,6 @@ import SpeedDial from "@mui/material/SpeedDial";
 import SpeedDialIcon from "@mui/material/SpeedDialIcon";
 import SpeedDialAction from "@mui/material/SpeedDialAction";
 import Container from "@mui/material/Container";
-// import DataTable from "./DataTable";
 
 export default function AdminHeader() {
   const [open, setOpen] = useState(false);
@@ -34,7 +33,6 @@ export default function AdminHeader() {
       <Container maxWidth="lg">
         <Grid
           container
-          // spacing={1}
           justifyContent="center"
           alignItems="center"
           direction="row"
@@ -98,6 +96,7 @@ export default function AdminHeader() {
               border: "1px inset rgba(0, 0, 0, .2)",
               padding: "25px 50px",
               margin: "20px 10px",
+
               ...glassEffect,
             }}>
             <TableRow>
@@ -133,7 +132,7 @@ export default function AdminHeader() {
                     <Box sx={{ transform: "translateZ(0px)" }}>
                       <Backdrop open={open} sx={{ background: "none" }} />
                       <SpeedDial
-                        direction="down"
+                        direction="up"
                         ariaLabel="SpeedDial tooltip"
                         icon={<SpeedDialIcon onClick={handleOpen} sx={{}} />}
                         open={open}
@@ -150,7 +149,7 @@ export default function AdminHeader() {
                             sx={{
                               position: !open ? "absolute" : "",
                               transition: open ? ".5s" : "none",
-
+                              borderRadius: "50%",
                               "&:hover": {
                                 color: "black",
                                 background: "azure",
