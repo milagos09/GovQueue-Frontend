bc29916673f3df32be9f13ef433639b6a55a4e58
420ku <mordecai51213@gmail.com>
2023-09-07 11:02:02 +0800

    add error boundary with fallback ui to app.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -7,33 +7,51 @@ import Home from "./pages/Home";
 // import Support from "./pages/public/Support";
 // import Login from "./pages/admin/Login";
 // import Dashboard from "./pages/public/Dashboard";
+import ErrorBoundary from "./components/ErrorBoundary";
+import { Box } from "@mui/material";
+
+const centerStyle = {
+  display: "flex",
+  justifyContent: "center",
+  alignItems: "center",
+  minHeight: "100vh",
+};
 
 export default function App() {
-    // const router = createBrowserRouter([
-    //     {
-    //         path: "/",
-    //         element: <Home />,
-    //         errorElement: <ErrorPage />,
-    //         children: [
-    //             {
-    //                 path: "dashboard",
-    //                 element: <Dashboard />,
-    //             },
-    //             {
-    //                 path: "about",
-    //                 element: <About />,
-    //             },
-    //             {
-    //                 path: "support",
-    //                 element: <Support />,
-    //             },
-    //         ],
-    //     },
-    //     { path: "login", element: <Login /> },
-    // ]);
-    return (
-        <>
-            <Home />
-        </>
-    );
+  // const router = createBrowserRouter([
+  //     {
+  //         path: "/",
+  //         element: <Home />,
+  //         errorElement: <ErrorPage />,
+  //         children: [
+  //             {
+  //                 path: "dashboard",
+  //                 element: <Dashboard />,
+  //             },
+  //             {
+  //                 path: "about",
+  //                 element: <About />,
+  //             },
+  //             {
+  //                 path: "support",
+  //                 element: <Support />,
+  //             },
+  //         ],
+  //     },
+  //     { path: "login", element: <Login /> },
+  // ]);
+  return (
+    <>
+      <Box style={centerStyle}>
+        <ErrorBoundary
+          fallback={
+            <Box>
+              <h1>Something went wrong</h1>
+            </Box>
+          }>
+          <Home />
+        </ErrorBoundary>
+      </Box>
+    </>
+  );
 }
---
+++ src/components/ErrorBoundary/index.jsx
@@ -0,0 +1,29 @@
+import React from "react";
+
+class ErrorBoundary extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = { hasError: false }; // change to true to check if fallback shows
+  }
+
+  static getDerivedStateFromError(error) {
+    // Update state so the next render will show the fallback UI.
+    return { hasError: true };
+  }
+
+  componentDidCatch(error, info) {
+    console.log(error, info);
+    // logErrorToMyService(error, info.componentStack);
+  }
+
+  render() {
+    if (this.state.hasError) {
+      // You can render any custom fallback UI
+      return this.props.fallback;
+    }
+
+    return this.props.children;
+  }
+}
+
+export default ErrorBoundary;
