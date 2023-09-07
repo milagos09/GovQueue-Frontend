c2985e44b27a6405603f348e56b136543bf5b963
unknown <milagos31@gmail.com>
2023-09-08 04:55:02 +0800

    apply date range filter on logs using actual backend server

--- src/components/AdminLogs/LogPicker.jsx
+++ src/components/AdminLogs/LogPicker.jsx
@@ -1,20 +1,13 @@
 import { Grid, Stack, Typography, Box } from "@mui/material";
 import { LocalizationProvider } from "@mui/x-date-pickers/LocalizationProvider";
 import { DemoContainer, DemoItem } from "@mui/x-date-pickers/internals/demo";
-import { useState } from "react";
 import { AdapterDayjs } from "@mui/x-date-pickers/AdapterDayjs";
-import dayjs from "dayjs";
 import { DatePicker } from "@mui/x-date-pickers/DatePicker";
 import MenuItem from "@mui/material/MenuItem";
 import FormControl from "@mui/material/FormControl";
 import Select from "@mui/material/Select";
 
-export default function LogPicker({ queues }) {
-    const [defaultDate, setDefaultDate] = useState(dayjs());
-    const currentDate = dayjs();
-    const defaultStartDate = currentDate.subtract(1, "day");
-    const [startDate, setStartDate] = useState(defaultStartDate);
-
+export default function LogPicker({ queues, startDate, endDate, handleChangeDate, selectedQueue, handleChangeSelect }) {
     return (
         <Box sx={{ padding: 2, margin: 2 }}>
             <Grid container direction="row" justifyContent="space-evenly" alignItems="center" spacing={2}>
@@ -40,7 +33,7 @@ export default function LogPicker({ queues }) {
                                         },
                                     }}
                                     value={startDate}
-                                    onChange={(newValue) => setStartDate(newValue)}
+                                    onChange={(newDate) => handleChangeDate("startDate", newDate)}
                                 />
                             </DemoItem>
                         </DemoContainer>
@@ -69,8 +62,8 @@ export default function LogPicker({ queues }) {
                                             fontSize: "0.8rem",
                                         },
                                     }}
-                                    value={defaultDate}
-                                    onChange={(newValue) => setDefaultDate(newValue)}
+                                    value={endDate}
+                                    onChange={(newDate) => handleChangeDate("endDate", newDate)}
                                 />
                             </DemoItem>
                         </DemoContainer>
@@ -90,15 +83,16 @@ export default function LogPicker({ queues }) {
                             size="small"
                             labelId="queue-simple-select-label"
                             id="queue-simple-select"
-                            defaultValue={"all"}
+                            value={selectedQueue}
                             sx={{ fontSize: ".84rem" }}
+                            onChange={handleChangeSelect}
                         >
-                            <MenuItem sx={{ fontSize: ".84rem" }} value={"all"}>
+                            <MenuItem sx={{ fontSize: ".84rem" }} value={"All"}>
                                 All
                             </MenuItem>
-                            {queues.map((queue, i) => (
-                                <MenuItem key={i} sx={{ fontSize: ".84rem" }} value={queue.name}>
-                                    {queue.name}
+                            {queues.map((queue) => (
+                                <MenuItem key={queue} sx={{ fontSize: ".84rem" }} value={queue}>
+                                    {queue}
                                 </MenuItem>
                             ))}
                         </Select>
