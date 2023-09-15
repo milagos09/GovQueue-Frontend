09bf5f86ea4e60fd4c999b95e7230be2f2ade568
johanblupojr <johanblupojr@gmail.com>
2023-09-15 16:36:48 +0800

    converted to get data from Json file

--- src/components/AdminSupport/AgencyDocumentation.jsx
+++ src/components/AdminSupport/AgencyDocumentation.jsx
@@ -1,75 +1,52 @@
 import { Light as SyntaxHighlighter } from "react-syntax-highlighter";
-import { dark, docco } from "react-syntax-highlighter/dist/esm/styles/hljs";
+import { dark } from "react-syntax-highlighter/dist/esm/styles/hljs";
 import { Divider, Typography } from "@mui/material";
-import agencyExample from "./JSON/getAgencyNodeJS.json";
+import agencyData from "./example.json"; // Import your JSON data here
 
-export default function AgecyDocumentation() {
-  const agencyIdexample = JSON.stringify(agencyExample, null, 2);
+export default function AgencyDocumentation() {
   return (
     <>
-      <div>
-        <Typography variant="h4" gutterBottom>
-          Agencies
-        </Typography>
+      {agencyData.map((agency, index) => (
+        <div key={index}>
+          <Typography variant="h4" gutterBottom>
+            {agency.title}
+          </Typography>
 
-        <Typography variant="h6" gutterBottom>
-          How to make an API call
-        </Typography>
+          <Typography variant="h6" gutterBottom>
+            {agency.description}
+          </Typography>
 
-        <SyntaxHighlighter style={dark}>
-          {"http://govqueue-api.onrender.com/agencies/{agencyId}"}
-        </SyntaxHighlighter>
+          {agency.content.map((content, contentIndex) => (
+            <div key={contentIndex}>
+              <Typography
+                variant="body1"
+                gutterBottom
+                style={{ marginTop: "20px" }}
+              >
+                Examples of {content.language} API call
+              </Typography>
 
-        <Typography variant="body1" gutterBottom style={{ marginTop: "20px" }}>
-          Examples of API call
-        </Typography>
+              <SyntaxHighlighter style={dark}>{content.req}</SyntaxHighlighter>
+            </div>
+          ))}
 
-        <SyntaxHighlighter language="curl" style={dark}>
-          {`cURL
-  curl --location 'http://govqueue-api.onrender.com/agencies/13'`}
-        </SyntaxHighlighter>
-        <Divider style={{ marginTop: "20px" }}></Divider>
-        <SyntaxHighlighter language="curl" style={dark}>
-          {`nodeJS
-  const http = require('follow-redirects').http;
-  const fs = require('fs');
-  
-  let options = {
-    'method': 'GET',
-    'hostname': 'govqueue-api.onrender.com',
-    'path': '/agencies/',
-    'headers': {
-    },
-    'maxRedirects': 20
-  };
-  
-  const req = http.request(options, (res) => {
-    let chunks = [];
-  
-    res.on("data", (chunk) => {
-      chunks.push(chunk);
-    });
-  
-    res.on("end", (chunk) => {
-      let body = Buffer.concat(chunks);
-      console.log(body.toString());
-    });
-  
-    res.on("error", (error) => {
-      console.error(error);
-    });
-  });
-  
-  req.end();`}
-        </SyntaxHighlighter>
-        <Typography variant="body1" gutterBottom style={{ marginTop: "20px" }}>
-          Examples of API response
-        </Typography>
-        <SyntaxHighlighter language="JSON" style={dark}>
-          {agencyIdexample}
-        </SyntaxHighlighter>
-        <Divider />
-      </div>
+          <Divider style={{ marginTop: "20px" }}></Divider>
+
+          <Typography
+            variant="body1"
+            gutterBottom
+            style={{ marginTop: "20px" }}
+          >
+            Examples of API response
+          </Typography>
+
+          <SyntaxHighlighter language="JSON" style={dark}>
+            {agency.res}
+          </SyntaxHighlighter>
+
+          <Divider />
+        </div>
+      ))}
     </>
   );
 }
