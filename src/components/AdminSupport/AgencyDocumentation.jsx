5ede5ab0e5ec032827ce4eb2b7d72de7de7531b8
johanblupojr <johanblupojr@gmail.com>
2023-09-16 11:32:21 +0800

    add design and agency data

--- src/components/AdminSupport/AgencyDocumentation.jsx
+++ src/components/AdminSupport/AgencyDocumentation.jsx
@@ -1,16 +1,14 @@
-import { Light as SyntaxHighlighter } from "react-syntax-highlighter";
-import { dark } from "react-syntax-highlighter/dist/esm/styles/hljs";
+import { Prism as SyntaxHighlighter } from "react-syntax-highlighter";
+import { a11yDark } from "react-syntax-highlighter/dist/esm/styles/prism";
 import { Divider, Typography, Select, MenuItem } from "@mui/material";
-import agencyData from "./example.json"; // Import your JSON data here
+import agencyData from "./example.json";
 import { useState } from "react";
 
 export default function AgencyDocumentation() {
-  // Initialize a state to track selected language for each agency
   const [selectedLanguages, setSelectedLanguages] = useState(
     agencyData.map(() => "curl")
   );
 
-  // Handler to update the selected language for a specific agency
   const handleLanguageChange = (index, language) => {
     const newSelectedLanguages = [...selectedLanguages];
     newSelectedLanguages[index] = language;
@@ -30,18 +28,18 @@ export default function AgencyDocumentation() {
             {agency.name}
           </Typography>
           <Typography gutterBottom>{agency.description}</Typography>
-          <SyntaxHighlighter style={dark} wrapLongLines={true}>
+          <SyntaxHighlighter style={a11yDark} wrapLongLines={true}>
             {agency.raw}
           </SyntaxHighlighter>
           <Typography variant="h5" gutterBottom>
             Example:
           </Typography>
-          {/* Select dropdown for language */}
+
           <Typography variant="h6" gutterBottom>
             Request:
           </Typography>
           <Select
-            value={selectedLanguages[index]} // Use the selected language for this agency
+            value={selectedLanguages[index]}
             onChange={(e) => handleLanguageChange(index, e.target.value)}
           >
             {agency.content.map((content, contentIndex) => (
@@ -50,19 +48,27 @@ export default function AgencyDocumentation() {
               </MenuItem>
             ))}
           </Select>
-          {/* Syntax Highlighter */}
           {agency.content.map((content, contentIndex) => {
             if (content.language === selectedLanguages[index]) {
               return (
                 <div key={contentIndex}>
-                  <SyntaxHighlighter style={dark} wrapLongLines={true}>
+                  <SyntaxHighlighter
+                    style={a11yDark}
+                    wrapLongLines={true}
+                    language={selectedLanguages[index]}
+                  >
                     {content.req}
                   </SyntaxHighlighter>
-                  {/* Add Example API Response section */}
+
                   <Typography variant="h6" gutterBottom>
                     Response:
                   </Typography>
-                  <SyntaxHighlighter style={dark} wrapLongLines={true}>
+
+                  <SyntaxHighlighter
+                    style={a11yDark}
+                    wrapLongLines={true}
+                    language="json"
+                  >
                     {agency.res}
                   </SyntaxHighlighter>
                   <Divider />
