b4b3d4c4f4a3771fbabec59e352dd7bebddfd404
unknown <milagos31@gmail.com>
2023-09-21 18:41:08 +0800

    added analyze button onClick event with sockets

--- src/components/AdminLogs/LogOptions.jsx
+++ src/components/AdminLogs/LogOptions.jsx
@@ -1,8 +1,31 @@
 import { Primary } from "./../Buttons/";
 import { Stack, Box, Switch, FormControlLabel, FormGroup } from "@mui/material";
 import DownloadCSVButton from "./DownloadCSVButton";
+import { socket } from "./../../helpers/socket";
+import { useState } from "react";
+import { useEffect } from "react";
 
 export default function LogOptions({ logs, agency }) {
+    const [svg, setSVG] = useState(null);
+
+    const analyze = () => {
+        console.log(logs);
+        const names = [...new Set(logs.map((log) => log.name))];
+        console.log(names);
+        let counts = [];
+        names.forEach((name) => {
+            counts.push({ name: name, count: logs.filter((log) => log.name == name).length });
+        });
+        console.log(counts);
+        socket.emit("analyzeLogs", { logs, counts });
+    };
+    useEffect(() => {
+        socket.on("analyzeLogs", (svgMarkup) => {
+            setSVG(svgMarkup);
+            // console.log(svgMarkup);
+        });
+    }, []);
+
     return (
         <Box sx={{ my: 4 }}>
             <Stack
@@ -17,9 +40,10 @@ export default function LogOptions({ logs, agency }) {
                 </FormGroup>
                 <Box>
                     <DownloadCSVButton logs={logs} agency={agency} />
-                    <Primary value={"Analyze"} />
+                    <Primary value={"Analyze"} onClick={analyze} />
                 </Box>
             </Stack>
+            <div dangerouslySetInnerHTML={{ __html: svg }} />
         </Box>
     );
 }
