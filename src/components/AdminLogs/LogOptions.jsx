255fa9c25c1d9745377ee8c56d28d7f2a473f8db
unknown <milagos31@gmail.com>
2023-09-28 19:40:37 +0800

    undo changes in logoptions

--- src/components/AdminLogs/LogOptions.jsx
+++ src/components/AdminLogs/LogOptions.jsx
@@ -1,40 +1,34 @@
 import { Primary } from "./../Buttons/";
 import { Stack, Box, Switch, FormControlLabel, FormGroup } from "@mui/material";
 import DownloadCSVButton from "./DownloadCSVButton";
-import { socket } from "./../../helpers/socket";
+import ChartModal from "./ChartModal";
 import { useState } from "react";
-import { useEffect } from "react";
 
 export default function LogOptions({ logs, agency }) {
-  const [showCharts, setShowCharts] = useState(false);
-  return (
-    <Box sx={{ my: 4 }}>
-      <Stack
-        direction={{ xs: "col", sm: "row" }}
-        justifyContent="space-between"
-        alignItems="center"
-        spacing={5}
-        rowGap={3}
-      >
-        <FormGroup>
-          <FormControlLabel
-            control={<Switch defaultChecked />}
-            label="Always Show Logs"
-          />
-        </FormGroup>
-        <Box>
-          <DownloadCSVButton logs={logs} agency={agency} />
-          <Primary
-            value={"Analyze"}
-            onClick={() => {
-              setShowCharts(true);
-            }}
-          />
-          {showCharts && (
-            <ChartModal setOpenCharts={setShowCharts} logs={logs} />
-          )}
+    const [showCharts, setShowCharts] = useState(false);
+    return (
+        <Box sx={{ my: 4 }}>
+            <Stack
+                direction={{ xs: "col", sm: "row" }}
+                justifyContent="space-between"
+                alignItems="center"
+                spacing={5}
+                rowGap={3}
+            >
+                <FormGroup>
+                    <FormControlLabel control={<Switch defaultChecked />} label="Always Show Logs" />
+                </FormGroup>
+                <Box>
+                    <DownloadCSVButton logs={logs} agency={agency} />
+                    <Primary
+                        value={"Analyze"}
+                        onClick={() => {
+                            setShowCharts(true);
+                        }}
+                    />
+                    {showCharts && <ChartModal setOpenCharts={setShowCharts} logs={logs} />}
+                </Box>
+            </Stack>
         </Box>
-      </Stack>
-    </Box>
-  );
+    );
 }
