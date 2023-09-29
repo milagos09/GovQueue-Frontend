1eb71783078909ad1c6db990c79d8dcef6ce4695
unknown <milagos31@gmail.com>
2023-09-29 17:48:56 +0800

    modify calculateTimeDifference to render now if less than 1min

--- src/helpers/calculateTimeDifference.js
+++ src/helpers/calculateTimeDifference.js
@@ -5,12 +5,17 @@ export default function calculateTimeDifference(timeString) {
     const timeDifferenceInMillis = Math.abs(currentTime - providedTime); // Use Math.abs to ensure non-negative difference
 
     const minutesDifference = Math.floor(timeDifferenceInMillis / (1000 * 60));
-    const hoursDifference = Math.floor(minutesDifference / 60);
-    const remainingMinutes = minutesDifference % 60;
 
-    if (hoursDifference > 0) {
-        return `${hoursDifference} hour${hoursDifference > 1 ? "s" : ""} ago`;
+    if (minutesDifference < 1) {
+        return "now";
     } else {
-        return `${remainingMinutes} min${remainingMinutes !== 1 ? "s" : ""} ago`;
+        const hoursDifference = Math.floor(minutesDifference / 60);
+        const remainingMinutes = minutesDifference % 60;
+
+        if (hoursDifference > 0) {
+            return `${hoursDifference} hour${hoursDifference > 1 ? "s" : ""} ago`;
+        } else {
+            return `${remainingMinutes} min${remainingMinutes !== 1 ? "s" : ""} ago`;
+        }
     }
 }
