0920d48217e9038b6179b8750885c3d4770a116b
unknown <milagos31@gmail.com>
2023-10-03 03:42:21 +0800

    used icons and custom break points

--- src/components/QueueActions/index.jsx
+++ src/components/QueueActions/index.jsx
@@ -1,73 +1,129 @@
-import { Box, Backdrop, Menu, MenuItem } from "@mui/material";
 import { useState } from "react";
-import Favorites from "./Favorites";
+import { Tooltip, Box, Backdrop, Menu, MenuItem, ListItemIcon, IconButton, Divider } from "@mui/material";
 import ShowLogs from "./ShowLogs";
-import EventNoteIcon from "@mui/icons-material/EventNote";
 import MoreHorizTwoToneIcon from "@mui/icons-material/MoreHorizTwoTone";
-import { roundIcon } from "../../themes/MyTheme";
+import FavoriteIcon from "@mui/icons-material/Favorite";
+import UnFavoriteIcon from "@mui/icons-material/FavoriteBorder";
+import EventNoteIcon from "@mui/icons-material/EventNote";
+import Filter1Icon from "@mui/icons-material/Filter1";
+import { gold, roundIcon } from "../../themes/MyTheme";
 
-export default function QueueActions({ agency, isFavorite, toggleFavorite }) {
-  const [anchorEl, setAnchorEl] = useState(null);
-  const [openLogs, setOpenLogs] = useState(false);
+export default function QueueActions({ agency, isFavorite, toggleFavorite, toggleQueues, customBreakPoint }) {
+    const [anchorEl, setAnchorEl] = useState(null);
+    const [openLogs, setOpenLogs] = useState(false);
+    const [isQueuesToggled, setIsQueuesToggled] = useState(false);
 
-  const handleOpen = (event) => setAnchorEl(event.currentTarget);
-  const handleClose = () => setAnchorEl(null);
+    const handleOpen = (event) => setAnchorEl(event.currentTarget);
+    const handleClose = () => setAnchorEl(null);
 
-  const handleToggleFavorite = () => {
-    toggleFavorite(agency.agency_id, isFavorite);
-    handleClose();
-  };
+    const toggleLogs = () => {
+        setOpenLogs(!openLogs);
+        setIsQueuesToggled(false); // Reset the Queues toggle when Logs are toggled
+    };
 
-  const actions = [
-    {
-      icon: (
-        <Favorites
-          id={agency.agency_id}
-          isFavorite={isFavorite}
-          toggleFavorite={toggleFavorite}
-        />
-      ),
-      name: "Favorite",
-    },
-    { icon: <EventNoteIcon onClick={() => setOpenLogs(true)} />, name: "Logs" },
-  ];
+    const toggleQueuesHandler = () => {
+        toggleQueues();
+        setIsQueuesToggled(!isQueuesToggled); // Toggle Queues state
+        setOpenLogs(false); // Close the Logs when Queues is clicked
+    };
 
-  return (
-    <>
-      <Box sx={{ transform: "translateZ(0px)" }}>
-        <Backdrop open={Boolean(anchorEl)} sx={{ background: "none" }} />
-        <MoreHorizTwoToneIcon
-          id="MoreHorizTwoToneIcon-button"
-          aria-controls={open ? "basic-menu" : undefined}
-          aria-haspopup="true"
-          aria-expanded={open ? "true" : undefined}
-          onClick={handleOpen}
-          style={{
-            ...roundIcon,
-          }}
-        ></MoreHorizTwoToneIcon>
-        <Menu
-          id="basic-menu"
-          anchorEl={anchorEl}
-          open={Boolean(anchorEl)}
-          onClose={handleClose}
-          MenuListProps={{
-            "aria-labelledby": "MoreHorizTwoToneIcon-button",
-          }}
-        >
-          <MenuItem onClick={handleToggleFavorite}>
-            {isFavorite ? "Unfavorite" : "Favorite"}
-          </MenuItem>
-          <MenuItem onClick={() => setOpenLogs(true)}>Logs</MenuItem>
-        </Menu>
-      </Box>
-      {openLogs && (
-        <ShowLogs
-          agencyId={agency.agency_id}
-          title={agency.name}
-          setOpenLogs={setOpenLogs}
-        />
-      )}
-    </>
-  );
+    const handleToggleFavorite = () => {
+        toggleFavorite(agency.agency_id, isFavorite);
+    };
+
+    const SpreadActions = () => {
+        return (
+            <>
+                <Tooltip title="Favorite" placement="top" arrow>
+                    <IconButton sx={{ ...roundIcon, mx: 1 }} onClick={handleToggleFavorite}>
+                        {isFavorite ? (
+                            <FavoriteIcon sx={{ ...gold }} fontSize="small" />
+                        ) : (
+                            <UnFavoriteIcon fontSize="small" />
+                        )}
+                    </IconButton>
+                </Tooltip>
+                <Tooltip title="Logs" placement="top" arrow>
+                    <IconButton sx={{ ...roundIcon, mx: 1 }} onClick={toggleLogs}>
+                        <EventNoteIcon
+                            sx={{ ...(openLogs && gold) }} // Change icon color to gold if openLogs is true
+                            fontSize="small"
+                        />
+                    </IconButton>
+                </Tooltip>
+                <Tooltip title="Queues" placement="top" arrow>
+                    <IconButton sx={{ ...roundIcon, mx: 1 }} onClick={toggleQueuesHandler}>
+                        <Filter1Icon
+                            sx={{ ...(isQueuesToggled && gold) }} // Change icon color to gold if isQueuesToggled is true
+                            fontSize="small"
+                        />
+                    </IconButton>
+                </Tooltip>
+            </>
+        );
+    };
+
+    return (
+        <>
+            {customBreakPoint ? (
+                <SpreadActions />
+            ) : (
+                <Box sx={{ transform: "translateZ(0px)" }}>
+                    <Backdrop open={Boolean(anchorEl)} sx={{ background: "none" }} />
+                    <IconButton
+                        onClick={handleOpen}
+                        sx={{
+                            ...roundIcon,
+                        }}
+                        aria-controls={open ? "basic-menu" : undefined}
+                        aria-haspopup="true"
+                        aria-expanded={open ? "true" : undefined}
+                    >
+                        <MoreHorizTwoToneIcon />
+                    </IconButton>
+                    <Menu
+                        dense
+                        id="basic-menu"
+                        anchorEl={anchorEl}
+                        open={Boolean(anchorEl)}
+                        onClose={handleClose}
+                        transformOrigin={{ horizontal: "right", vertical: "top" }}
+                        anchorOrigin={{ horizontal: "left", vertical: "center" }}
+                    >
+                        <MenuItem onClick={handleToggleFavorite}>
+                            <ListItemIcon>
+                                {isFavorite ? (
+                                    <FavoriteIcon sx={{ ...gold }} fontSize="small" />
+                                ) : (
+                                    <UnFavoriteIcon fontSize="small" />
+                                )}
+                            </ListItemIcon>
+                            Favorite
+                        </MenuItem>
+                        <Divider />
+                        <MenuItem onClick={toggleLogs}>
+                            <ListItemIcon>
+                                <EventNoteIcon
+                                    sx={{ ...(openLogs && gold) }} // Change icon color to gold if openLogs is true
+                                    fontSize="small"
+                                />
+                            </ListItemIcon>
+                            Logs
+                        </MenuItem>
+                        <Divider />
+                        <MenuItem onClick={toggleQueuesHandler}>
+                            <ListItemIcon>
+                                <Filter1Icon
+                                    sx={{ ...(isQueuesToggled && gold) }} // Change icon color to gold if isQueuesToggled is true
+                                    fontSize="small"
+                                />
+                            </ListItemIcon>
+                            Queues
+                        </MenuItem>
+                    </Menu>
+                </Box>
+            )}
+            {openLogs && <ShowLogs agencyId={agency.agency_id} title={agency.name} setOpenLogs={toggleLogs} />}
+        </>
+    );
 }
--- src/components/QueueTable/Row.jsx
+++ src/components/QueueTable/Row.jsx
@@ -1,102 +1,56 @@
-import { useState } from "react";
 import Box from "@mui/material/Box";
 import Collapse from "@mui/material/Collapse";
-import IconButton from "@mui/material/IconButton";
 import TableCell from "@mui/material/TableCell";
 import TableRow from "@mui/material/TableRow";
-import KeyboardArrowDownIcon from "@mui/icons-material/KeyboardArrowDown";
-import KeyboardArrowUpIcon from "@mui/icons-material/KeyboardArrowUp";
-import Windows from "../Windows";
-import QueueActions from "../QueueActions";
-import { roundIcon } from "../../themes/MyTheme";
+import { useState } from "react";
 import { Link } from "react-router-dom";
+import QueueActions from "../QueueActions";
+import Windows from "../Windows";
 
-export default function Row({
-  agency,
-  customBreakPoint,
-  isFavorite,
-  toggleFavorite,
-}) {
-  const [open, setOpen] = useState(false);
-  const fontSize = customBreakPoint ? ".9rem" : ".75rem";
-  const maxWidth = customBreakPoint ? "80px" : "60px";
+export default function Row({ agency, customBreakPoint, isFavorite, toggleFavorite }) {
+    const [showQueues, setShowQueues] = useState(false);
 
-  return (
-    <>
-      <TableRow
-        aria-label="expand row"
-        size="small"
-        onClick={() => setOpen(!open)}
-        sx={{
-          "&:hover *": {
-            color: "black",
-          },
-        }}
-      >
-        {
-          // <TableCell align="center">
-          //   <IconButton
-          //   aria-label="expand row"
-          //   size="small"
-          //   onClick={() => setOpen(!open)}
-          //   sx={{
-          //     "&:hover *": {
-          //       color: "black",
-          //     },
-          //   }}
-          // >
-          //   {open ? (
-          //     <KeyboardArrowUpIcon
-          //       style={{
-          //         ...roundIcon,
-          //       }}
-          //     />
-          //   ) : (
-          //     <KeyboardArrowDownIcon
-          //       style={{
-          //         ...roundIcon,
-          //       }}
-          //     />
-          //   )}
-          //   </IconButton>
-          // </TableCell>
-        }
-        <TableCell component="th" scope="row" sx={{ textAlign: "center" }}>
-          <Link target="_blank" to={`agency/${agency.agency_id}`}>
-            <img
-              src={agency.logo}
-              loading="lazy"
-              style={{ maxWidth: maxWidth, borderRadius: "50%" }}
-            />
-          </Link>
-        </TableCell>
-        <TableCell align="center" sx={{ fontSize: fontSize }}>
-          <Link target="_blank" to={`agency/${agency.agency_id}`}>
-            {agency.name}
-          </Link>
-        </TableCell>
-        {customBreakPoint && (
-          <TableCell align="center">{agency.region}</TableCell>
-        )}
-        <TableCell align="center">
-          <QueueActions
-            agency={agency}
-            isFavorite={isFavorite}
-            boolean
-            toggleFavorite={toggleFavorite}
-            function
-          />
-        </TableCell>
-      </TableRow>
-      <TableRow>
-        <TableCell style={{ paddingBottom: 0, paddingTop: 0 }} colSpan={5}>
-          <Collapse in={open} timeout="auto" unmountOnExit>
-            <Box sx={{ marginBottom: "20px" }}>
-              <Windows minWidth={80} queues={agency.queues} />
-            </Box>
-          </Collapse>
-        </TableCell>
-      </TableRow>
-    </>
-  );
+    const toggleQueues = () => {
+        setShowQueues((prev) => !prev);
+    };
+
+    const fontSize = ".9rem";
+
+    const logoWidth = "80px";
+
+    return (
+        <>
+            <TableRow aria-label="expand row" size="small">
+                <TableCell component="th" scope="row" sx={{ textAlign: "center" }}>
+                    <Link target="_blank" to={`agency/${agency.agency_id}`}>
+                        <img src={agency.logo} loading="lazy" style={{ maxWidth: logoWidth, borderRadius: "50%" }} />
+                    </Link>
+                </TableCell>
+                <TableCell align="center" sx={{ fontSize: fontSize }}>
+                    <Link target="_blank" to={`agency/${agency.agency_id}`}>
+                        {agency.name}
+                    </Link>
+                </TableCell>
+                {customBreakPoint && <TableCell align="center">{agency.region}</TableCell>}
+                <TableCell align="center">
+                    <QueueActions
+                        customBreakPoint={customBreakPoint}
+                        agency={agency}
+                        isFavorite={isFavorite}
+                        toggleFavorite={toggleFavorite}
+                        toggleQueues={toggleQueues}
+                    />
+                </TableCell>
+            </TableRow>
+            <TableRow>
+                <TableCell style={{ paddingBottom: 0, paddingTop: 0 }} colSpan={5}>
+                    <Collapse in={showQueues} timeout="auto" unmountOnExit>
+                        <Box sx={{ marginBottom: "20px" }}>
+                            <Windows minWidth={80} queues={agency.queues} />
+                        </Box>
+                    </Collapse>
+                </TableCell>
+            </TableRow>
+        </>
+    );
 }
