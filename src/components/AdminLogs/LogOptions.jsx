33851a2b8239b30574450c626701390e751e5468
johanblupojr <johanblupojr@gmail.com>
2023-10-05 23:48:29 +0800

    adjust mobile view for admin logs

--- src/components/AdminLogs/LogOptions.jsx
+++ src/components/AdminLogs/LogOptions.jsx
@@ -5,30 +5,35 @@ import ChartModal from "./ChartModal";
 import { useState } from "react";
 
 export default function LogOptions({ logs, agency }) {
-    const [showCharts, setShowCharts] = useState(false);
-    return (
-        <Box sx={{ my: 4 }}>
-            <Stack
-                direction={{ xs: "col", sm: "row" }}
-                justifyContent="space-between"
-                alignItems="center"
-                spacing={5}
-                rowGap={3}
-            >
-                <FormGroup>
-                    <FormControlLabel control={<Switch defaultChecked />} label="Always Show Logs" />
-                </FormGroup>
-                <Box>
-                    <DownloadCSVButton logs={logs} agency={agency} />
-                    <Primary
-                        value={"Analyze"}
-                        onClick={() => {
-                            setShowCharts(true);
-                        }}
-                    />
-                    {showCharts && <ChartModal setOpenCharts={setShowCharts} logs={logs} />}
-                </Box>
-            </Stack>
+  const [showCharts, setShowCharts] = useState(false);
+  return (
+    <Box sx={{ my: 4 }}>
+      <Stack
+        direction={{ xs: "col", sm: "row" }}
+        justifyContent="space-between"
+        alignItems="center"
+        spacing={5}
+        rowGap={3}
+      >
+        <FormGroup>
+          <FormControlLabel
+            control={<Switch defaultChecked />}
+            label="Always Show Logs"
+          />
+        </FormGroup>
+        <Box>
+          <DownloadCSVButton logs={logs} agency={agency} />
+          <Primary
+            value={"Analyze"}
+            onClick={() => {
+              setShowCharts(true);
+            }}
+          />
+          {showCharts && (
+            <ChartModal setOpenCharts={setShowCharts} logs={logs} />
+          )}
         </Box>
-    );
+      </Stack>
+    </Box>
+  );
 }
--- src/components/AdminLogs/index.jsx
+++ src/components/AdminLogs/index.jsx
@@ -12,91 +12,94 @@ const initialStartDate = dayjs();
 const initialEndDate = dayjs().add(1, "day");
 
 export default function AdminLogs() {
-    const URL = import.meta.env.VITE_SERVER_URL;
-    const user = getSessionStorage("user");
-    const { height } = CheckScreenSize();
+  const URL = import.meta.env.VITE_SERVER_URL;
+  const user = getSessionStorage("user");
+  const { height } = CheckScreenSize();
 
-    const [state, setState] = useState({
-        logs: [],
-        filteredLogs: [],
-        startDate: initialStartDate,
-        endDate: initialEndDate,
-        selectedQueue: "All",
+  const [state, setState] = useState({
+    logs: [],
+    filteredLogs: [],
+    startDate: initialStartDate,
+    endDate: initialEndDate,
+    selectedQueue: "All",
+    isFetching: false,
+  });
+
+  // Fetch logs from the API and update component state.
+  const fetchLogs = async (agencyId, startDate, endDate) => {
+    try {
+      setState((prevState) => ({ ...prevState, isFetching: true }));
+      const result = await fetch(
+        `${URL}/logs/dateRange/?agencyId=${agencyId}&startDate=${startDate.format(
+          "YYYY-MM-DD"
+        )}&endDate=${endDate.format("YYYY-MM-DD")}`
+      );
+      const logs = await result.json();
+      setState((prevState) => ({
+        ...prevState,
+        logs,
+        filteredLogs: logs,
         isFetching: false,
-    });
+      }));
+    } catch (error) {
+      console.error("Error fetching logs:", error);
+      setState((prevState) => ({ ...prevState, isFetching: false }));
+    }
+  };
 
-    // Fetch logs from the API and update component state.
-    const fetchLogs = async (agencyId, startDate, endDate) => {
-        try {
-            setState((prevState) => ({ ...prevState, isFetching: true }));
-            const result = await fetch(
-                `${URL}/logs/dateRange/?agencyId=${agencyId}&startDate=${startDate.format(
-                    "YYYY-MM-DD"
-                )}&endDate=${endDate.format("YYYY-MM-DD")}`
-            );
-            const logs = await result.json();
-            setState((prevState) => ({
-                ...prevState,
-                logs,
-                filteredLogs: logs,
-                isFetching: false,
-            }));
-        } catch (error) {
-            console.error("Error fetching logs:", error);
-            setState((prevState) => ({ ...prevState, isFetching: false }));
-        }
-    };
+  // Handle changes to the selected date and trigger log fetching.
+  const handleChangeDate = async (setter, newDate) => {
+    let startDateToUpdate, endDateToUpdate;
 
-    // Handle changes to the selected date and trigger log fetching.
-    const handleChangeDate = async (setter, newDate) => {
-        let startDateToUpdate, endDateToUpdate;
+    if (setter === "startDate") {
+      startDateToUpdate = newDate;
+      endDateToUpdate = state.endDate;
+    } else {
+      startDateToUpdate = state.startDate;
+      endDateToUpdate = newDate;
+    }
 
-        if (setter === "startDate") {
-            startDateToUpdate = newDate;
-            endDateToUpdate = state.endDate;
-        } else {
-            startDateToUpdate = state.startDate;
-            endDateToUpdate = newDate;
-        }
+    setState((prevState) => ({
+      ...prevState,
+      startDate: startDateToUpdate,
+      endDate: endDateToUpdate,
+      selectedQueue: "All",
+    }));
 
-        setState((prevState) => ({
-            ...prevState,
-            startDate: startDateToUpdate,
-            endDate: endDateToUpdate,
-            selectedQueue: "All",
-        }));
+    await fetchLogs(user.agency_id, startDateToUpdate, endDateToUpdate);
+  };
 
-        await fetchLogs(user.agency_id, startDateToUpdate, endDateToUpdate);
-    };
+  // Handle changes to the selected queue and update filtered logs.
+  const handleChangeSelect = (e) => {
+    const name = e.target.value;
+    setState((prevState) => ({ ...prevState, selectedQueue: name }));
+    const filteredLogs =
+      name === "All"
+        ? state.logs
+        : state.logs.filter((log) => log.name === name);
+    setState((prevState) => ({ ...prevState, filteredLogs }));
+  };
 
-    // Handle changes to the selected queue and update filtered logs.
-    const handleChangeSelect = (e) => {
-        const name = e.target.value;
-        setState((prevState) => ({ ...prevState, selectedQueue: name }));
-        const filteredLogs = name === "All" ? state.logs : state.logs.filter((log) => log.name === name);
-        setState((prevState) => ({ ...prevState, filteredLogs }));
-    };
+  // Fetch logs from the API when the component is initially mounted.
+  useEffect(() => {
+    fetchLogs(user.agency_id, state.startDate, state.endDate);
+  }, []);
 
-    // Fetch logs from the API when the component is initially mounted.
-    useEffect(() => {
-        fetchLogs(user.agency_id, state.startDate, state.endDate);
-    }, []);
-
-    return (
-        <>
-            <Container maxWidth="lg" sx={{ minHeight: `${height - 190}px` }}>
-                <LogPicker
-                    queues={[...new Set(state.logs.map((log) => log.name))]}
-                    startDate={state.startDate}
-                    endDate={state.endDate}
-                    handleChangeDate={handleChangeDate}
-                    selectedQueue={state.selectedQueue}
-                    handleChangeSelect={handleChangeSelect}
-                />
-                <LogTable logs={state.filteredLogs} />
-                <LogOptions logs={state.filteredLogs} agency={user.agencyDetails} />
-            </Container>
-            <LoadingScreen isFetching={state.isFetching} />
-        </>
-    );
+  return (
+    <>
+      <Container maxWidth="lg" sx={{ minHeight: `${height - 190}px` }}>
+        <LogPicker
+          queues={[...new Set(state.logs.map((log) => log.name))]}
+          startDate={state.startDate}
+          endDate={state.endDate}
+          handleChangeDate={handleChangeDate}
+          selectedQueue={state.selectedQueue}
+          handleChangeSelect={handleChangeSelect}
+        />
+        <LogTable logs={state.filteredLogs} />
+        <LogOptions logs={state.filteredLogs} agency={user.agencyDetails} />
+      </Container>
+      <LoadingScreen isFetching={state.isFetching} />
+    </>
+  );
 }
