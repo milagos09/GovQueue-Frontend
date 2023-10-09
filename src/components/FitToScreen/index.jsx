d22dd5def80ba39a6129adb4fc5fe5b7ace236e4
unknown <milagos31@gmail.com>
2023-10-10 03:13:05 +0800

    implement fit to screen and adjust admin support page

---
+++ src/components/FitToScreen/index.jsx
@@ -0,0 +1,8 @@
+import { CheckScreenSize } from "../../hooks/CheckScreenSize";
+
+export default function FitToScreen({ reduce = 182, children }) {
+    const { height } = CheckScreenSize();
+
+    const minHeight = height - reduce;
+    return <div style={{ minHeight }}>{children}</div>;
+}
--- src/pages/AdminPage/AdminDashboard.jsx
+++ src/pages/AdminPage/AdminDashboard.jsx
@@ -1,11 +1,9 @@
 import AdminHome from "../../components/AdminHome/index";
-import { Outlet } from "react-router-dom";
 
 export default function AdminDashboard() {
     return (
         <>
             <AdminHome />
-            <Outlet />
         </>
     );
 }
