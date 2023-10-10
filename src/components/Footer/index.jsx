eaed9afd07699ff6388adbb5c62378cf1dd712f7
johanblupojr <johanblupojr@gmail.com>
2023-10-10 21:42:44 +0800

    update footer links

--- src/components/Footer/index.jsx
+++ src/components/Footer/index.jsx
@@ -8,88 +8,112 @@ import { CheckScreenSize } from "../../hooks/CheckScreenSize";
 import { dark } from "./../../themes/MyTheme";
 
 export default function Footer() {
-    const { width } = CheckScreenSize();
+  const { width } = CheckScreenSize();
+  const recipientEmail = "govqueue@gmail.com";
+  const handleIconClick = () => {
+    window.location.href = `mailto:${recipientEmail}`;
+  };
 
-    return (
-        <Box sx={{ ...dark, paddingY: "10px", boxShadow: 3 }}>
-            <Grid container alignItems="center">
-                <Grid item xs={4} sm={2} md={3} align="center" order={0}>
-                    <img
-                        src={GovQueueLogo}
-                        alt="App logo"
-                        width="80"
-                        height="80"
-                        style={{
-                            marginRight: "10px",
-                        }}
-                    />
-                </Grid>
-                <Grid item xs={12} sm={6} md={6} align="center" order={width < 600 ? 2 : 1}>
-                    <Typography
-                        variant="subtitle1"
-                        color="textaction"
-                        align="center"
-                        sx={{
-                            textAlign: "center",
-                        }}
-                    >
-                        Copyright © {new Date().getFullYear()}, GovQueue. All rights reserved.
-                    </Typography>
-                </Grid>
-                <Grid item xs={7} sm={4} md={3} align="center" order={width < 600 ? 1 : 2}>
-                    <Stack flexDirection="row" justifyContent="space-evenly" flexWrap="wrap" sx={{ color: "azure" }}>
-                        <Link
-                            href="https://www.google.com/"
-                            target="_blank"
-                            sx={{
-                                "&:hover *": {
-                                    color: "#FB9300",
-                                },
-                            }}
-                        >
-                            <GoogleIcon fontSize="large" style={{ color: "azure" }} />
-                        </Link>
-                        <Link
-                            href="https://www.facebook.com/"
-                            target="_blank"
-                            sx={{
-                                "&:hover *": {
-                                    color: "#FB9300",
-                                },
-                            }}
-                        >
-                            <FacebookIcon fontSize="large" style={{ color: "azure" }} />
-                        </Link>
-                        <Link
-                            href="https://github.com/"
-                            target="_blank"
-                            sx={{
-                                "&:hover *": {
-                                    color: "#FB9300",
-                                },
-                            }}
-                        >
-                            <GitHubIcon fontSize="large" style={{ color: "azure" }} />
-                        </Link>
-                        <Link
-                            href="https://www.youtube.com/"
-                            target="_blank"
-                            sx={{
-                                "&:hover *": {
-                                    color: "#FB9300",
-                                },
-                            }}
-                        >
-                            <YouTubeIcon
-                                fontSize="large"
-                                style={{
-                                    color: "azure",
-                                }}
-                            />
-                        </Link>
-                    </Stack>
-                </Grid>
-            </Grid>
-        </Box>
-    );
+  return (
+    <Box sx={{ ...dark, paddingY: "10px", boxShadow: 3 }}>
+      <Grid container alignItems="center">
+        <Grid item xs={4} sm={2} md={3} align="center" order={0}>
+          <img
+            src={GovQueueLogo}
+            alt="App logo"
+            width="80"
+            height="80"
+            style={{
+              marginRight: "10px",
+            }}
+          />
+        </Grid>
+        <Grid
+          item
+          xs={12}
+          sm={6}
+          md={6}
+          align="center"
+          order={width < 600 ? 2 : 1}
+        >
+          <Typography
+            variant="subtitle1"
+            color="textaction"
+            align="center"
+            sx={{
+              textAlign: "center",
+            }}
+          >
+            Copyright © {new Date().getFullYear()}, GovQueue. All rights
+            reserved.
+          </Typography>
+        </Grid>
+        <Grid
+          item
+          xs={7}
+          sm={4}
+          md={3}
+          align="center"
+          order={width < 600 ? 1 : 2}
+        >
+          <Stack
+            flexDirection="row"
+            justifyContent="space-evenly"
+            flexWrap="wrap"
+            sx={{ color: "azure" }}
+          >
+            <Link
+              href="#"
+              onClick={handleIconClick}
+              sx={{
+                "&:hover *": {
+                  color: "#FB9300",
+                },
+              }}
+            >
+              <GoogleIcon fontSize="large" style={{ color: "azure" }} />
+            </Link>
+            <Link
+              href="https://www.facebook.com/profile.php?id=100094291845554"
+              target="_blank"
+              sx={{
+                "&:hover *": {
+                  color: "#FB9300",
+                },
+              }}
+            >
+              <FacebookIcon fontSize="large" style={{ color: "azure" }} />
+            </Link>
+            <Link
+              href="https://github.com/milagos09/GovQueue-Frontend"
+              target="_blank"
+              sx={{
+                "&:hover *": {
+                  color: "#FB9300",
+                },
+              }}
+            >
+              <GitHubIcon fontSize="large" style={{ color: "azure" }} />
+            </Link>
+            <Link
+              href="https://www.youtube.com/@GovQueue"
+              target="_blank"
+              sx={{
+                "&:hover *": {
+                  color: "#FB9300",
+                },
+              }}
+            >
+              <YouTubeIcon
+                fontSize="large"
+                style={{
+                  color: "azure",
+                }}
+              />
+            </Link>
+          </Stack>
+        </Grid>
+      </Grid>
+    </Box>
+  );
 }
